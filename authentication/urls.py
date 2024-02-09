754965d5deabbaafdb54462c7d2a8a7854775d81
Denis Nganga <denisnganga16@gmail.com>
2024-02-09 13:39:50 +0300

    defining of the whole register function, adding favicon, updating stylesheet and templates

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import Register
 
+app_name='authentication'
+
 urlpatterns = [
     path('register/', Register, name='register'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def Register(request):
-    #register view
+    if request.method=='POST':
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        confirmPassword=request.POST.get('confirmPassword')
+
+            #checking if the passwords match
+        if password != confirmPassword:
+            error_message="Passwords do not match"
+            return render(request, 'register.html',{'error_message':error_message})
+        
+        #checking if the username exists
+
+        if User.objects.filter (username=username).exists():
+            error_message="The user already exists. Use another username"
+            return render (request, 'register.html', {'error_message':error_message})
+     
+        #checking if the email exists
+
+        if User.objects.filter(email=email).exists():
+            error_message="The email address is registered under an account"
+            return render(request, 'register.html',{'error_message':error_message})
+        
+        #creating a new user
+        new_user= User.objects.create_user(username=username, email=email, password=password)
+        new_user.save()
+
+        messages.success(request,"An account was successfully registered")
+    
     return render(request,'register.html')
 
 
