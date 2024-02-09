6da532b0e881634e44cd4d4521fa2bc9c07c780b
Denis Nganga <denisnganga16@gmail.com>
2024-02-09 08:35:06 +0300

    static js file for registration template

---
+++ static/js/register.js
@@ -0,0 +1,11 @@
+function validateForm() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+  
+    if (password != confirmPassword) {
+      alert("Passwords do not match");
+      return false;
+    }
+    return true;
+  }
+  
