0f306c05faa45f1edbb66d9ae0518a3f81b649c3
Denis Nganga <denisnganga16@gmail.com>
2024-02-10 20:35:05 +0300

    created the forms form and the profile model modification

--- authentication/models.py
+++ authentication/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class profile(models.Model):
+class Profile(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     profile_picture=models.ImageField(upload_to='profile_pics', null=True,blank=True)
     full_name=models.CharField(max_length=100)
