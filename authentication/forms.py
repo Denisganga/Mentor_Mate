55da4ed1836f9bacdd881669ac3116393f6353ea
Denis Nganga <denisnganga16@gmail.com>
2024-02-11 21:03:33 +0300

    created the forms file for the Profile model

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,3 +1,17 @@
 from django import forms
 from .models import Profile
 
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+
+        fields=[
+            'profile_picture',
+            'full_name',
+            'is_mentor',
+            'is_mentee',
+            'education',
+            'skills',
+            'years_of_experience',
+            'mentorship_skills'
+        ]
