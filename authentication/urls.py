950575d6216bbc97cd5cc5e3d93691909d75ccc5
Denis Nganga <denisnganga16@gmail.com>
2024-02-11 21:34:23 +0300

    url pattern for the profile

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Register,Homepage
+from .views import Register,Homepage,Profile
 
 app_name='authentication'
 
 urlpatterns = [
     path('register/', Register, name='register'),
     path('homepage/', Homepage, name='homepage'),
+    path('profile/', Profile, name='profile'),
 ]
