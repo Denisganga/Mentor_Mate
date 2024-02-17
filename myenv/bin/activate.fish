59e9bf47d059e23daa579fcc6c6c2ff6486b4dcc
Denis Nganga <denisnganga16@gmail.com>
2024-02-17 09:24:49 +0300

    modification

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/denis/Desktop/personalprojects/MentorMate/MentorMate/myenv"
+set -gx VIRTUAL_ENV "/home/denis/personalprojects/Mentor_Mate/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
