7b72afcc6687f2e57a4301a118d7655149caa157
Denis Nganga <denisnganga16@gmail.com>
2024-02-27 02:55:30 +0300

    setting the media

--- MentorMate/settings.py
+++ MentorMate/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 STATIC_URL = '/static/'
 
