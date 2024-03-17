c00592dfb9e24188929c1cc31939cc63a95436b0
Denis Nganga <denisnganga16@gmail.com>
2024-03-17 10:46:54 +0300

    class form of the allumni

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,5 +1,22 @@
 from django import forms
-from .models import Profile
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile,Alumni,Student,School
+
+class AlumniRegistrationForm(UserCreationForm):
+    school=forms.ModelChoiceField(School.objects.all(), empty_label='select a school')
+    graduation_year=forms.IntegerField(label='Graduation Year')
+    bio=forms.CharField(widget=forms.Textarea(attrs={'rows':4}), required=False)
+    course=forms.CharField(max_length=100, label='Area of study')
+
+    class Meta:
+        model=Alumni
+        fields=[
+            'school',
+            'graduation_year',
+            'bio',
+            'course'
+        ]
+
 
 class ProfileForm(forms.ModelForm):
     class Meta:
