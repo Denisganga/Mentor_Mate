28ce74ee69c555e9c2c27d1ee7c43fd1aed48a63
Denis Nganga <denisnganga16@gmail.com>
2024-03-17 10:14:57 +0300

    reggistering my models on the admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Profile,slider
+from .models import Profile,slider,School,Student,Mentorship
 
 admin.site.register(Profile)
 admin.site.register(slider)
+admin.site.register(School)
+admin.site.register(Student)
+admin.site.register(Mentorship)
 
 # Register your models here.
